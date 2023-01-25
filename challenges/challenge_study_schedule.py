d273f1649f002e66db644425bab93fb4eae0008d
Igor Leal <igorhenriqueleal3@gmail.com>
2023-01-25 16:20:56 -0300

    feat: Requisito 01 concluído

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) is not int:
+        return None
+
+    count = 0
+
+    for get_entrance, get_exit in permanence_period:
+        if type(get_entrance) is not int or type(get_exit) is not int:
+            return None
+        if get_entrance <= target_time <= get_exit:
+            count += 1
+
+    return count
